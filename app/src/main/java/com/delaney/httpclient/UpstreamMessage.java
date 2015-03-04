2c7deef7dc44a2161a4ec92a4aa63e084ef2c776
Emmet <emmet.delaney9@mail.dcu.ie>
2015-03-04 17:33:15 +0000

    -added more code for upstream POST requests

--- app/src/main/java/com/delaney/httpclient/UpstreamMessage.java
+++ app/src/main/java/com/delaney/httpclient/UpstreamMessage.java
@@ -7,129 +7,104 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class UpstreamMessage {
 
     private static final String PARAMETER_REG_ID = "regId";
     private static final String PARAMETER_MOBILE = "mobile";
+    private static final String PARAMETER_FRIENDS_LIST = "friendsList";
+    private static final Logger logger = Logger.getLogger(UpstreamMessage.class.getName());
 
     public static void postRegister(String gcmId, String mobile) {
-        // Create a new HttpClient and Post Header
         HttpClient httpclient = new DefaultHttpClient();
         HttpPost httppost = new HttpPost("https://www.igneous-equinox-653.appspot.com/register");
-
         try {
-            // Add your data
             List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
             nameValuePairs.add(new BasicNameValuePair(PARAMETER_REG_ID, gcmId));
             nameValuePairs.add(new BasicNameValuePair(PARAMETER_MOBILE, mobile));
             httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
-            // Execute HTTP Post Request
             HttpResponse response = httpclient.execute(httppost);
-
         } catch (Exception e) {
 
         }
     }
 
     public static void postUnregister(String gcmId) {
-        // Create a new HttpClient and Post Header
         HttpClient httpclient = new DefaultHttpClient();
         HttpPost httppost = new HttpPost("https://www.igneous-equinox-653.appspot.com/unregister");
-
         try {
-            // Add your data
             List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
             nameValuePairs.add(new BasicNameValuePair(PARAMETER_REG_ID, gcmId));
             httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
-            // Execute HTTP Post Request
             HttpResponse response = httpclient.execute(httppost);
-
         } catch (Exception e) {
 
         }
     }
 
     public static void postAdd(String gcmId, String friendsAdded) {
-        // Create a new HttpClient and Post Header
         HttpClient httpclient = new DefaultHttpClient();
         HttpPost httppost = new HttpPost("https://www.igneous-equinox-653.appspot.com/add");
-
         try {
-            // Add your data
             List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
             nameValuePairs.add(new BasicNameValuePair(PARAMETER_REG_ID, gcmId));
-            nameValuePairs.add(new BasicNameValuePair("stringdata", "AndDev is Cool!"));
+            nameValuePairs.add(new BasicNameValuePair(PARAMETER_FRIENDS_LIST, friendsAdded));
             httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
-            // Execute HTTP Post Request
             HttpResponse response = httpclient.execute(httppost);
-
         } catch (Exception e) {
 
         }
     }
 
-    public static void postRemove() {
-        // Create a new HttpClient and Post Header
+    public static void postRemove(String gcmId, String friendsRemoved) {
         HttpClient httpclient = new DefaultHttpClient();
         HttpPost httppost = new HttpPost("https://www.igneous-equinox-653.appspot.com/remove");
-
         try {
-            // Add your data
             List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-            nameValuePairs.add(new BasicNameValuePair("id", "12345"));
-            nameValuePairs.add(new BasicNameValuePair("stringdata", "AndDev is Cool!"));
+            nameValuePairs.add(new BasicNameValuePair(PARAMETER_REG_ID, gcmId));
+            nameValuePairs.add(new BasicNameValuePair(PARAMETER_FRIENDS_LIST, friendsRemoved));
             httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
-            // Execute HTTP Post Request
             HttpResponse response = httpclient.execute(httppost);
-
         } catch (Exception e) {
 
         }
     }
 
-    public static void postVisibility(String visibility, String friendIds) {
-        // Create a new HttpClient and Post Header
+    public static void postVisibility(String gcmId, String visibility, String friendIds) {
         HttpClient httpclient = new DefaultHttpClient();
-        HttpPost httppost = new HttpPost("https://www.igneous-equinox-653.appspot.com/script.php");
-
+        HttpPost httppost = new HttpPost("https://www.igneous-equinox-653.appspot.com/visibility");
         try {
-            // Add your data
-            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-            nameValuePairs.add(new BasicNameValuePair("id", "12345"));
-            nameValuePairs.add(new BasicNameValuePair("stringdata", "AndDev is Cool!"));
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
+            nameValuePairs.add(new BasicNameValuePair(PARAMETER_REG_ID, gcmId));
+            nameValuePairs.add(new BasicNameValuePair(PARAMETER_FRIENDS_LIST, friendIds));
+            nameValuePairs.add(new BasicNameValuePair("visibility", visibility));
             httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
-            // Execute HTTP Post Request
             HttpResponse response = httpclient.execute(httppost);
-
         } catch (Exception e) {
 
         }
     }
 
-    public static void postUpdate() {
-        // Create a new HttpClient and Post Header
+    public static void postUpdate(String gcmId, String location) {
         HttpClient httpclient = new DefaultHttpClient();
-        HttpPost httppost = new HttpPost("https://www.igneous-equinox-653.appspot.com/add");
+        HttpPost httppost = new HttpPost("https://www.igneous-equinox-653.appspot.com/update");
 
         try {
-            // Add your data
             List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-            nameValuePairs.add(new BasicNameValuePair("id", "12345"));
-            nameValuePairs.add(new BasicNameValuePair("stringdata", "AndDev is Cool!"));
+            nameValuePairs.add(new BasicNameValuePair(PARAMETER_REG_ID, gcmId));
+            nameValuePairs.add(new BasicNameValuePair("location", location));
             httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
-            // Execute HTTP Post Request
             HttpResponse response = httpclient.execute(httppost);
-
         } catch (Exception e) {
 
         }
