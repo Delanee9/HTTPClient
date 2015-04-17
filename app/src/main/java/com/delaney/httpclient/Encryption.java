b8032e12556c0a503b7823ecef547131827b5dfc
Emmet <emmet.delaney9@mail.dcu.ie>
2015-04-17 13:36:57 +0100

    - Added string hashing to fix security bug

---
+++ app/src/main/java/com/delaney/httpclient/Encryption.java
@@ -0,0 +1,26 @@
+package com.delaney.httpclient;
+
+import com.delaney.httpclient.errorHandling.ErrorHandling;
+
+import java.security.MessageDigest;
+
+class Encryption {
+
+    public static String encryptFunction(String input) {
+        try {
+            MessageDigest messageDigest = java.security.MessageDigest.getInstance("MD5");
+            messageDigest.update(input.getBytes());
+            byte [] byteDigest = messageDigest.digest();
+
+            StringBuilder hexString = new StringBuilder();
+            for(byte token : byteDigest) {
+                hexString.append(Integer.toHexString(0xFF & token));
+            }
+
+            return hexString.toString();
+        } catch (Exception e) {
+            new ErrorHandling("Failure in encryption", e.toString()).execute();
+            return "";
+        }
+    }
+}
